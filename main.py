28683fd638e5a2a3aecbb3ff28d1c512711010ce
TrenAza <akshaysanal2035@gmail.com>
2024-05-23 16:51:34 +0530

    Attendance calculator

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#A RECIPE APP
+#Attendence Calculator
+
+def attendence(classes,leaves):
+    if leaves <= 0:
+        print('Attend some classes dumb nigga')
+    if leaves > classes:
+        print('Bruh the hell')
+    
+    percentage = (leaves/classes)*100
+    return(percentage)
+classes = int(input('Total Number of Classes This Year'))
+leaves = int(input('Total Number of Leaves Taken This Year'))
+Total_percent = attendence(classes,leaves)
